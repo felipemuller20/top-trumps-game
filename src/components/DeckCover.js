ce15abcaac8cbdad55a9e78a34b9293ebb01bc92
Felipe Muller <felipeavmuller@gmail.com>
2021-12-23 21:46:04 -0300

    Renderiza os decks

--- src/components/DeckCover.js
+++ src/components/DeckCover.js
@@ -3,8 +3,8 @@ import React from 'react';
 function DeckCover({ image, name }) {
   return(
     <div>
-      <img src={image} alt={`${name} cover`} />
       <h3>{ name }</h3>
+      <img src={image} alt={`${name} cover`} />
     </div>
   )
 };
--- src/pages/Decks.js
+++ src/pages/Decks.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import DeckCover from '../components/DeckCover';
+import DeckContext from '../context/DeckContext';
+import { fetchDecks } from '../services/fetchApi';
 
 function Decks() {
+  const { setDecks, decks } = useContext(DeckContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getDecks = async () => {
+      const result = await fetchDecks();
+      setDecks(result);
+    };
+    getDecks();
+  }, [])
+
+  useEffect(() => {
+    if (decks.length > 0) {
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+    }
+  }, [decks]);
+
   return(
     <>
       <h1>Super Trunfo</h1>
@@ -10,7 +31,17 @@ function Decks() {
         para criar um novo!
       </h2>
       <div>
-        
+        {
+          isLoading ? 'Carregando baralhos...' : (
+            decks.map((deck, index) => (
+              <DeckCover
+                key={ index }
+                name={ deck.name }
+                image={ deck.coverImg }
+              />
+            ))
+          )
+        }
       </div>
     </>
   )
