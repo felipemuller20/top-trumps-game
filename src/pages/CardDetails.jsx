8cdf24047c325feed40675fd453066c54c964610
Felipe Muller <felipeavmuller@gmail.com>
2021-12-24 13:00:59 -0300

    Criado o estado inicial da pagina de detalhes

--- src/pages/CardDetails.jsx
+++ src/pages/CardDetails.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import DeckContext from '../context/DeckContext';
+import { fetchCards } from '../services/fetchApi';
 
 function CardDetails({ match: { params: { id } } }) {
+  const { decks } = useContext(DeckContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [cards, setCards] = useState([]);
+
+  useState(() => {
+    const getCards = async () => {
+      const result = await fetchCards();
+      setCards(result);
+    };
+    getCards();
+  }, []);
+
+  useState(() => {
+    if (cards.length > 0) {
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+    }
+  }, [cards])
+
   return(
-    <h1>Detalhes {id}</h1>
+    <h1>Detalhes {typeof id}</h1>
   )
 }
 
