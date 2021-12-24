7b2d3c6d5b150f935c34c8a9fc6e83ee777a7f20
Felipe Muller <felipeavmuller@gmail.com>
2021-12-23 23:57:17 -0300

    Adicionado style basico

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { Switch, Route } from "react-router-dom";
 import CreateDeck from './pages/CreateDeck';
 import Decks from './pages/Decks';
--- src/components/DeckCover.js
+++ src/components/DeckCover.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function DeckCover({ image, name }) {
+  const click = () => {
+    console.log('teste');
+  }
   return(
-    <div className="coverBorder">
-      <h3 className="coverName">{ name }</h3>
+    <div className="coverContainer" onClick={click }>
       <img className="coverImg" src={image} alt={`${name} cover`} />
+      <button className="coverName">{ name }</button>
     </div>
   )
 };
